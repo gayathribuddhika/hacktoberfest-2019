9e84f58592cf5ae81ed16f2dcf35bc857eceb105
Amaya Hewavithana <39590745+InduAmaya@users.noreply.github.com>
2019-10-18 18:35:52 +0530

    Create java_helloworld_InduAmaya.java

---
+++ scripts/java_helloworld_InduAmaya.java
@@ -0,0 +1,9 @@
+// LANGUAGE: java
+// ENV: java
+// AUTHOR: Amaya
+// GITHUB: https://github.com/InduAmaya
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
